b3f4ec5e2cdebd13e67214b3fc3294445c5d7085
Jesse Chan <jc@lineageos.org>
2020-06-13 11:52:12 +0800

    power-libperfmgr: simply wait if idle state node is not present
    Change-Id: I93dd97e5437761d2d969f93a5af0cae864fd0a65
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- power-libperfmgr/disp-power/InteractionHandler.cpp
+++ power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -94,15 +94,14 @@ bool InteractionHandler::Init() {
     if (mState != INTERACTION_STATE_UNINITIALIZED)
         return true;
 
-    int fd = FbIdleOpen();
-    if (fd < 0)
-        return false;
-    mIdleFd = fd;
+    mIdleFd = FbIdleOpen();
 
     mEventFd = eventfd(0, EFD_NONBLOCK);
     if (mEventFd < 0) {
         ALOGE("Unable to create event fd (%d)", errno);
-        close(mIdleFd);
+        if (mIdleFd >= 0) {
+            close(mIdleFd);
+        }
         return false;
     }
 
@@ -125,7 +124,9 @@ void InteractionHandler::Exit() {
     mThread->join();
 
     close(mEventFd);
-    close(mIdleFd);
+    if (mIdleFd >= 0) {
+        close(mIdleFd);
+    }
 }
 
 void InteractionHandler::PerfLock() {
@@ -233,6 +234,18 @@ void InteractionHandler::WaitForIdle(int32_t wait_ms, int32_t timeout_ms) {
         return;
     } else if (ret < 0) {
         ALOGE("%s: error in poll while waiting", __func__);
+        return;
+    }
+
+    if (mIdleFd < 0) {
+        ret = poll(pfd, 1, timeout_ms);
+        if (ret > 0) {
+            ALOGV("%s: wait for duration aborted", __func__);
+            return;
+        } else if (ret < 0) {
+            ALOGE("%s: Error on waiting for duration (%zd)", __func__, ret);
+            return;
+        }
         return;
     }
 
