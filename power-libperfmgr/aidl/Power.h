3a7f70d7f0e33612b832c219e9f1afe462867623
Jesse Chan <jc@lineageos.org>
2020-06-29 12:26:29 +0800

    power-libperfmgr: remove Google-specific VR and camera hints
    Change-Id: Iac0e65a16f660583d3400a35622113d35d8d1d27

--- power-libperfmgr/aidl/Power.h
+++ power-libperfmgr/aidl/Power.h
@@ -53,7 +53,6 @@ class Power : public ::aidl::android::hardware::power::BnPower {
   private:
     std::shared_ptr<DisplayLowPower> mDisplayLowPower;
     std::unique_ptr<InteractionHandler> mInteractionHandler;
-    std::atomic<bool> mVRModeOn;
     std::atomic<bool> mSustainedPerfModeOn;
 };
 
